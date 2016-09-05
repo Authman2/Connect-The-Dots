3d47e713c8bfc0fc4594e2ca80c2a1520e88078f
Adeola Uthman <authman2@gmail.com>
2016-09-04 20:18:31 -0400

    Added more methods for creating the list of points to connect

--- ConnectTheDots/src/MAIN/Connect.java
+++ ConnectTheDots/src/MAIN/Connect.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 public class Connect {
 	
 	// An arraylist that contains all of the points that need to be connected.
-	ArrayList<Point> points;
+	private ArrayList<Point> points;
 	
 
 	/** Creates an empty object with no points to connect. */
@@ -19,5 +19,20 @@ public class Connect {
 	
 	
 	
-
+	///////// Methods ///////////
+	
+	/** Sets the list of points to have connected to each other. 
+	 * @param points -- The list of coordinate points that need to be connected. */
+	public void setPoints(ArrayList<Point> points) { this.points = points; }
+	
+	
+	/** Adds a point to the list of points that must be connected. 
+	 * @param p -- The point to add to the list of points. */
+	public void addPoint(Point p) { this.points.add(p); }
+	
+	
+	/** Adds a point to the list of points with the specified x and y coordinate points.
+	 * @param x -- The x coordinate. 
+	 * @param y -- The y coordinate. */
+	public void addPoint(int x, int y) { this.points.add(new Point(x, y)); }
 }
