cbd22ce5e672469f77bbda31938dbcc39ce915f5
Adeola Uthman <authman2@gmail.com>
2016-09-04 20:00:19 -0400

    Began work on point connecting algorithm

--- ConnectTheDots/src/MAIN/Connect.java
+++ ConnectTheDots/src/MAIN/Connect.java
@@ -1,9 +1,23 @@
 package MAIN;
 
-public class Connect {
+import java.awt.Point;
+import java.util.ArrayList;
 
-	public Connect() {
-		
-	}
+public class Connect {
+	
+	// An arraylist that contains all of the points that need to be connected.
+	ArrayList<Point> points;
+	
+
+	/** Creates an empty object with no points to connect. */
+	public Connect() { points = new ArrayList<Point>(); }
+	
+	
+	/** Creates a new Connect object and initializes it with a list of points to connect to each other.
+	 * @param points -- The list of coordinate points that need to be connected. */
+	public Connect(ArrayList<Point> points) { this.points = points; }
+	
+	
+	
 
 }
