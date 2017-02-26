fc1b66997bb2c97f3eea1c25884109ea81d3f182
Adeola Uthman <authman2@gmail.com>
2017-02-26 13:31:51 -0500

    Updated the point connector

--- ConnectTheDots/src/MAIN/ConnectTheDots.java
+++ ConnectTheDots/src/MAIN/ConnectTheDots.java
@@ -1,14 +1,19 @@
 package MAIN;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.util.ArrayList;
+import java.util.Random;
 
 import javax.swing.JFrame;
 
-import MAIN.PointConnector.ConnectionType;
+import je.other.pointconnector.DrawingPanel;
+import je.other.pointconnector.Point;
+import je.other.pointconnector.PointConnector;
+
 
 public class ConnectTheDots {
-
+	
 	public static void main(String[] args) {
 		
 		// Make a JFrame to display the points
@@ -17,45 +22,33 @@ public class ConnectTheDots {
 		tester.setLocationRelativeTo(null);
 		tester.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		
 		// Create a list of points
-		ArrayList<Point> points = new ArrayList<Point>();
-		points.add(new Point(5,20));
-		points.add(new Point(5,15));
-		points.add(new Point(13,25));
-		points.add(new Point(100,70));
-		points.add(new Point(27,5));
-		points.add(new Point(80,90));
-		points.add(new Point(15,13));
-		points.add(new Point(135,120));
-		points.add(new Point(110,110));
-		points.add(new Point(89,135));
-		points.add(new Point(5,150));
-		points.add(new Point(89,90));
-		points.add(new Point(340,200));
-		points.add(new Point(280,239));
-		points.add(new Point(290,320));
-		points.add(new Point(270,90));
-		points.add(new Point(299,90));
-		points.add(new Point(89,230));
-		points.add(new Point(100,256));
-		points.add(new Point(45,324));
+		ArrayList<Point> points = createRandomPoints();
 		
+		// Point connector
+		PointConnector pc = configurePointConnector(points);
 
 		// Create the panel that draws all of the points from the list above
-		DrawingPanel panel = new DrawingPanel(points, true);
+		DrawingPanel panel = new DrawingPanel(pc.getOutput(), true);
 		panel.setBackground(Color.white);
-		tester.add(panel);
-		
-		
-		// Create a new PointConnector object
-		PointConnector pointConnector = new PointConnector(points);
-		pointConnector.connectBy(ConnectionType.PROXIMITY, 100);
-		
-		
-		
+		tester.add(panel, BorderLayout.CENTER);
 		
 		tester.setVisible(true);
 	}
 
+	
+	public static ArrayList<Point> createRandomPoints() {
+		ArrayList<Point> p = new ArrayList<Point>();
+		Random rand = new Random();
+		int size = new Integer("50");
+		for(int i = 0; i < size; i++) p.add(new Point( rand.nextInt(300), rand.nextInt(300) ));
+		return p;
+	}
+	
+	public static PointConnector configurePointConnector(ArrayList<Point> points) {
+		PointConnector pc = new PointConnector(points);
+		pc.connect();
+		return pc;
+	}
+	
 }
